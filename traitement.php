e03bf916c1da7235c18400e87a8cdcc678793824
ikaran96 <ilaydakaran@yahoo.fr>
2020-03-31 09:56:33 +0200

    traitement mail

--- traitement.php
+++ traitement.php
@@ -1,23 +1,23 @@
 <?php
 
-  include ('connectBDD.php');
-
   $username = !empty($_POST['username']) ? $_POST['username'] : NULL;
   $user_firstname = !empty($_POST['user_firstname']) ? $_POST['user_firstname'] : NULL;
   $user_mail = !empty($_POST['user_mail']) ? $_POST['user_mail'] : NULL;
   $user_message= !empty($_POST['user_message']) ? $_POST['user_message'] : NULL;
+  $headers = "From: $username $user_firstname" . "\r\n" .
+  "CC: $user_mail";
 
-  $sql = $bdd->prepare ("INSERT INTO Messagr (Nom, Prenom, Email, Msg)
-                        VALUES (:username, :user_firstname, :user_mail, :user_message)");
+  $to = "karan@simplon-charleville.fr";
+  $subject = "Nouveau message";
 
-  $sql->execute(array(
-      'username' => $username,
-      'user_firstname' => $user_firstname,
-      'user_mail' => $user_mail,
-      'user_message' => $user_message
-  ));
+  mail($to,$subject, $user_message,$headers);
 
-  $sql-> closeCursor();
+
+
+
+
   header('location:index.php');
 
 ?>
+
+

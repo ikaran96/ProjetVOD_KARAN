b2b5d2516093c07799a71f877e71e1d3a46b352d
ikaran96 <59538431+ikaran96@users.noreply.github.com>
2020-04-06 08:08:33 +0200

    Add files via upload

--- traitement/connexion-user.php
+++ traitement/connexion-user.php
@@ -1,4 +1,32 @@
-<?php 
- 
+<?php
+
+    include ('../include/connectBDD.php');
+
+    $username = !empty($_POST['username']) ? $_POST['username'] : NULL;
+    $password = !empty($_POST['password_user']) ? $_POST['password_user'] : NULL;
+
+    $user = $bdd->prepare ('SELECT * FROM User WHERE Pseudo="'.$username.'"');
+    $user ->execute();
+    
+    $member = $user->fetch();
+
+    $mdpval = password_verify($password, $member['Mdp']);
+
+
+
+    if (!$member) {
+        header('location:../connexion.php?error=login');
+    } else {
+        if ($mdpval) {
+        session_start();
+        $_SESSION['id_user'] = $member['id_user'];
+        $_SESSION['Pseudo'] = $member['Pseudo'];
+        $_SESSION['Mdp'] = $member['Mdp'];
+        $_SESSION['id_typeuser'] = $member['id_typeuser'];
+        header('location: ../admin.php');
+        } else {
+            header('location:../connexion.php?error=marchepas');
+        }
+    }
     
 ?>
